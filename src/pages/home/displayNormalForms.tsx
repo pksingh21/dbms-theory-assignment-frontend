6cc00d19ddd8acbe89ec578399b574ea0eb8abfd
Prateek Kumar Singh <20cs01003@iitbbs.ac.in>
2023-02-28 15:18:20 +0530

    Finally Completed !!

--- src/pages/home/displayNormalForms.tsx
+++ src/pages/home/displayNormalForms.tsx
@@ -15,7 +15,11 @@ export type KindsOfNormalForm = {
   "3NF": [boolean, number[]];
   BCNF: [boolean, number[]];
 };
-
+export type OtherKindsOfNormalForm = {
+  is2NF: [boolean, number[]];
+  is3NF: [boolean, number[]];
+  isBCNF: [boolean, number[]];
+};
 export type responseObject = {
   minimal_cover?: FunctiotnalDependency[];
   candidate_keys?: string[][];
@@ -31,32 +35,83 @@ type Props = {
   query: string;
 };
 
-export function CenteredGrid(completeData: CompleteDataAboutNormalizedForm) {
+export function CenteredGrid2(
+  completeData: FunctiotnalDependency,
+  heading1: string,
+  heading2: string
+) {
+  return (
+    <Grid container spacing={2} justifyContent="center">
+      <Grid item xs={12}>
+        <Typography color="primary" variant="h6" align="center">
+          {heading1}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h6" color="primary" align="center">
+          {heading2}
+        </Typography>
+      </Grid>
+      <Grid item xs={5}>
+        <Typography variant="subtitle1" align="center">
+          <Grid container spacing={2} justifyContent="center">
+            {completeData.left.map((fd, index) => (
+              <Grid item key={index}>
+                <Chip label={fd} />
+              </Grid>
+            ))}
+          </Grid>
+        </Typography>
+      </Grid>
+      <Grid item xs={2}>
+        <Typography variant="subtitle1" align="center">
+          <ArrowForwardIcon />
+        </Typography>
+      </Grid>
+      <Grid item xs={5}>
+        <Typography variant="subtitle1" align="center">
+          <Grid container spacing={2} justifyContent="center">
+            {completeData.right.map((fd, index) => (
+              <Grid item key={index}>
+                <Chip label={fd} />
+              </Grid>
+            ))}
+          </Grid>
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+}
+export function CenteredGrid(
+  completeData: CompleteDataAboutNormalizedForm,
+  heading1: string,
+  heading2: string
+) {
   console.log(completeData, "completeData");
   return (
     <Grid container spacing={2} justifyContent="center">
       <Grid item xs={12}>
         <Typography color="primary" variant="h6" align="center">
-          Attributes
+          {heading1}
         </Typography>
       </Grid>
       <Grid container spacing={2} justifyContent="center">
-        {completeData.attributes.map((attribute) => (
-          <Grid item>
+        {completeData.attributes.map((attribute, index) => (
+          <Grid item key={index}>
             <Chip label={attribute} />
           </Grid>
         ))}
       </Grid>
       <Grid item xs={12}>
         <Typography variant="h6" color="primary" align="center">
-          Functional Dependencies
+          {heading2}
         </Typography>
       </Grid>
       <Grid item xs={5}>
         <Typography variant="subtitle1" align="center">
           <Grid container spacing={2} justifyContent="center">
-            {completeData.fds.map((fd) => (
-              <Grid item>
+            {completeData.fds.map((fd, index) => (
+              <Grid item key={index}>
                 <Chip label={fd.left} />
               </Grid>
             ))}
@@ -71,8 +126,8 @@ export function CenteredGrid(completeData: CompleteDataAboutNormalizedForm) {
       <Grid item xs={5}>
         <Typography variant="subtitle1" align="center">
           <Grid container spacing={2} justifyContent="center">
-            {completeData.fds.map((fd) => (
-              <Grid item>
+            {completeData.fds.map((fd, index) => (
+              <Grid item key={index}>
                 <Chip label={fd.right} />
               </Grid>
             ))}
@@ -87,13 +142,17 @@ function displayNormalForms(props: Props) {
   if (props.normalFormData === undefined) {
     return <div></div>;
   }
-  const isNormalForm = {};
-
+  let isNormalForm: OtherKindsOfNormalForm = {
+    is2NF: [false, []],
+    is3NF: [false, []],
+    isBCNF: [false, []],
+  };
   if (props.normalFormData.normal_form !== undefined) {
-    for (let [key, value] of props.normalFormData.normal_form) {
-      console.log(key, value, "key, value");
-    }
+    isNormalForm["is2NF"] = props.normalFormData.normal_form["2NF"];
+    isNormalForm["is3NF"] = props.normalFormData.normal_form["3NF"];
+    isNormalForm["isBCNF"] = props.normalFormData.normal_form["BCNF"];
   }
+  console.log(isNormalForm, "isNormalForm");
   return (
     <>
       <Typography
@@ -109,30 +168,56 @@ function displayNormalForms(props: Props) {
       {/* "Normalize to 2NF", "Normalize to 3NF", "Normalize to BCNF", */}
       {props.query === "Normalize to BCNF" &&
         props.normalFormData.BCNF_Form?.map((normalForm, index) => (
-          <>{CenteredGrid(normalForm)}</>
+          <>
+            {CenteredGrid(normalForm, "Attributes", "Functional Dependencies")}
+          </>
         ))}
       {props.query === "Normalize to 3NF" &&
         props.normalFormData.NFForm_3?.map((normalForm, index) => (
-          <>{CenteredGrid(normalForm)}</>
+          <>
+            {CenteredGrid(normalForm, "Attributes", "Functional Dependencies")}
+          </>
         ))}
       {props.query === "Normalize to 2NF" &&
         props.normalFormData.NFForm_2?.map((normalForm, index) => (
-          <>{CenteredGrid(normalForm)}</>
+          <>
+            {CenteredGrid(normalForm, "Attributes", "Functional Dependencies")}
+          </>
         ))}
-      {console.log(props.normalFormData.normal_form, "props.normalFormData")}
       {props.query === "Check Normal Form" && (
         <>
-          {/* <Stack spacing={2}>
-            <Alert
-              severity={
-                props.normalFormData.normal_form?['2NF'][0] ? "success" : "error"
-              }
-            >
-              {props.normalFormData.normal_form?['2NF'][0] ? "2NF is satisfied" : "2NF is not satisfied"}
+          <Stack spacing={2}>
+            <Alert severity={isNormalForm.is2NF[0] ? "success" : "error"}>
+              {isNormalForm.is2NF[0] ? "2NF" : "Not 2NF"}
             </Alert>
-         </Stack> */}
+            <Alert severity={isNormalForm.is3NF[0] ? "success" : "error"}>
+              {isNormalForm.is3NF[0] ? "3NF" : "Not 3NF"}
+            </Alert>
+            <Alert severity={isNormalForm.isBCNF[0] ? "success" : "error"}>
+              {isNormalForm.isBCNF[0] ? "BCNF" : "Not BCNF"}
+            </Alert>
+          </Stack>
         </>
       )}
+      {props.query === "Find Candidate Keys" && (
+        <>
+          <Typography variant="subtitle1" align="center">
+            <Grid container spacing={2} justifyContent="center">
+              {props.normalFormData.candidate_keys?.map(
+                (candidate_keys, index) => (
+                  <Grid item key={index}>
+                    <Chip label={candidate_keys} />
+                  </Grid>
+                )
+              )}
+            </Grid>
+          </Typography>
+        </>
+      )}
+      {props.query === "Find Minimal Cover" &&
+        props.normalFormData.minimal_cover?.map((fd) => (
+          <>{CenteredGrid2(fd, "Minimal Cover", "")}</>
+        ))}
     </>
   );
 }
